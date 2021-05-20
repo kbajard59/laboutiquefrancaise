5aa546b127decf9f3d40518c0890e070f918d1fb
sylar59 <kevin.bajard@hotmail.com>
2021-05-20 08:27:41 +0200

    Création de la page d'inscription

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegisterController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->entityManager = $entityManager;
+    }
+    /**
+     * @Route("/inscription", name="register")
+     */
+    public function index(Request $request): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $user = $form->getData();
+
+            $entityManager = $this->entityManager;
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+        return $this->render('register/index.html.twig',array('form'=>$form->createView()));
+    }
+}
