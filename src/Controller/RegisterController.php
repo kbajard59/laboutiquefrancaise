9d0b453405863ded4e6a2b78422efc39c27529ba
sylar59 <kevin.bajard@hotmail.com>
2021-05-20 08:41:44 +0200

    Encodage du password

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegisterController extends AbstractController
 {
@@ -20,7 +21,7 @@ class RegisterController extends AbstractController
     /**
      * @Route("/inscription", name="register")
      */
-    public function index(Request $request): Response
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
@@ -28,6 +29,9 @@ class RegisterController extends AbstractController
 
         if($form->isSubmitted() && $form->isValid()){
             $user = $form->getData();
+
+            $password = $encoder->encodePassword($user,$user->getPassword());
+            $user->setPassword($password);
 
             $entityManager = $this->entityManager;
             $entityManager->persist($user);
