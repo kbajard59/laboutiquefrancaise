1a955b47f2171e1bb5c64d0f81d9bc1825316b64
sylar59 <kevin.bajard@hotmail.com>
2021-05-21 18:28:05 +0200

    Création du ProductCrudController

---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name'),
+            SlugField::new('slug')->setTargetFieldName('name'),
+            ImageField::new('illustration')
+                ->setBasePath('uploads/') //Chemin où les fichiers seront enregistrés
+                ->setUploadDir('public/uploads') // Chemin complet
+                ->setUploadedFileNamePattern('[randomhash].[extension]') //randomhash pour généré chaine aléatoire - extension pour garder l'extension
+                ->setRequired(false),
+            TextField::new('subtitle'),
+            TextareaField::new('description'),
+            MoneyField::new('price')->setCurrency('EUR'),
+            AssociationField::new('category') //CollectionField pour un champs pouvant avoir plusieurs category dans ce cas
+        ];
+    }
+
+}
