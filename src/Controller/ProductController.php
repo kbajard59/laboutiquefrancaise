a12038b05f9cfbed395a89fab6dd3f265460b75d
sylar59 <kevin.bajard@hotmail.com>
2021-05-26 00:39:13 +0200

    Réglage problème du footer + Amélioration des appels de requête dans le ProductController.php

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -24,8 +24,6 @@ class ProductController extends AbstractController
      */
     public function index(Request $request): Response
     {
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
-
         $search = new Search();
         $form = $this->createForm(SearchType::class,$search);
         
@@ -33,6 +31,8 @@ class ProductController extends AbstractController
 
         if($form->isSubmitted() && $form->isValid()){
             $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        }else{
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
         }
 
         return $this->render('product/index.html.twig',[
